383a101a27b4b5cf73f5fcccaa60794c6a6bd2fe
sh5914 <e6395316@gmail.com>
2026-07-07 18:31:53 +0900

    Add files via upload

--- players.js
+++ players.js
@@ -3,37 +3,63 @@ const playersData = [
     id: "player_masaki",
     display_text: "1番 ファーストベースマン 正木智也<br>背番号31 東京都大田区出身",
     typing_kana: "いちばん ふぁーすとべーすまん まさきともや<br>せばんごうさんじゅういち とうきょうとおおたくしゅっしん",
-    bgm_file: "audio/masaki.mp3",
+    bgm_file: "audio/masaki_cut.mp3",
     bgm_delay_ms: 2000
   },
   {
     id: "player_yanagimachi",
     display_text: "2番 ライトフィールダー 柳町達<br>背番号32 茨城県稲敷市出身",
     typing_kana: "にばん らいとふぃーるだー やなぎまちたつる<br>せばんごうさんじゅうに いばらきけんいなしきししゅっしん",
-    bgm_file: "audio/yanagimachi.mp3",
+    bgm_file: "audio/yanagimachi_cut.mp3",
     bgm_delay_ms: 2000
   },
   {
     id: "player_kondo",
     display_text: "3番 レフトフィールダー 近藤健介<br>背番号3 千葉県千葉市出身",
     typing_kana: "さんばん れふとふぃーるだー こんどうけんすけ<br>せばんごうさん ちばけんちばししゅっしん",
-    bgm_file: "audio/kondo.mp3",
+    bgm_file: "audio/kondo_cut.mp3",
     bgm_delay_ms: 2000
   },
   {
     id: "player_kurihara",
     display_text: "4番 サードベースマン 栗原陵矢<br>背番号24 福井県福井市出身",
     typing_kana: "よんばん さーどべーすまん くりはらりょうや<br>せばんごうにじゅうよん ふくいけんふくいししゅっしん",
-    bgm_file: "audio/kurihara.mp3",
+    bgm_file: "audio/kurihara_cut.mp3",
     bgm_delay_ms: 2000
   },
   {
     id: "player_yanagita",
     display_text: "5番 指名打者 柳田悠岐<br>背番号9 広島県広島市出身",
-    // ※指名打者は「デジグネイテッドヒッター(DH)」とするか迷いましたが、日本の球場アナウンスで一般的な「しめいだしゃ」のままにしています。
-    // もし変更したい場合は「でじぐねいてっどひったー」に書き換えてください。
     typing_kana: "ごばん しめいだしゃ やなぎたゆうき<br>せばんごうきゅう ひろしまけんひろしまししゅっしん",
-    bgm_file: "audio/yanagita.mp3",
+    bgm_file: "audio/yanagita_cut.mp3",
     bgm_delay_ms: 2000
+  },
+  {
+    id: "player_makihara",
+    display_text: "6番 セカンドベースマン 牧原大成<br>背番号8 福岡県久留米市出身",
+    typing_kana: "ろくばん せかんどべーすまん まきはらたいせい<br>せばんごうはち ふくおかけんくるめししゅっしん",
+    bgm_file: "audio/makihara_cut.mp3",
+    bgm_delay_ms: 2000
+  // },
+  // {
+  //   id: "player_shuto",
+  //   display_text: "7番 センターフィールダー 周東右京<br>背番号23 群馬県太田市出身",
+  //   typing_kana: "ななばん せんたーふぃーるだー しゅうとううきょう<br>せばんごうにじゅうさん ぐんまけんおおたししゅっしん",
+  //   bgm_file: "audio/shuto.mp3",
+  //   bgm_delay_ms: 2000
+  // },
+  // {
+  //   id: "player_umino",
+  //   display_text: "8番 キャッチャー 海野隆司<br>背番号62 岡山県岡山市出身",
+  //   typing_kana: "はちばん きゃっちゃー うみのたかし<br>せばんごうろくじゅうに おかやまけんおかやまししゅっしん",
+  //   bgm_file: "audio/umino.mp3",
+  //   bgm_delay_ms: 2000
+  // },
+  // {
+  //   id: "player_nomura",
+  //   display_text: "9番 ショートストップ 野村勇<br>背番号99 兵庫県神戸市出身",
+  //   typing_kana: "きゅうばん しょーとすとっぷ のむらいさみ<br>せばんごうきゅうじゅうきゅう ひょうごけんこうべししゅっしん",
+  //   bgm_file: "audio/nomura.mp3",
+  //   bgm_delay_ms: 2000
   }
 ];

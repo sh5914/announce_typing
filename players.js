38927a3a2401acb03fbe8e4d438e528aeb33ecc5
sh5914 <e6395316@gmail.com>
2026-07-07 15:22:41 +0900

    Add files via upload

---
+++ players.js
@@ -0,0 +1,39 @@
+const playersData = [
+  {
+    id: "player_masaki",
+    display_text: "1番 ファーストベースマン 正木智也<br>背番号31 東京都大田区出身",
+    typing_kana: "いちばん ふぁーすとべーすまん まさきともや<br>せばんごうさんじゅういち とうきょうとおおたくしゅっしん",
+    bgm_file: "audio/masaki.mp3",
+    bgm_delay_ms: 2000
+  },
+  {
+    id: "player_yanagimachi",
+    display_text: "2番 ライトフィールダー 柳町達<br>背番号32 茨城県稲敷市出身",
+    typing_kana: "にばん らいとふぃーるだー やなぎまちたつる<br>せばんごうさんじゅうに いばらきけんいなしきししゅっしん",
+    bgm_file: "audio/yanagimachi.mp3",
+    bgm_delay_ms: 2000
+  },
+  {
+    id: "player_kondo",
+    display_text: "3番 レフトフィールダー 近藤健介<br>背番号3 千葉県千葉市出身",
+    typing_kana: "さんばん れふとふぃーるだー こんどうけんすけ<br>せばんごうさん ちばけんちばししゅっしん",
+    bgm_file: "audio/kondo.mp3",
+    bgm_delay_ms: 2000
+  },
+  {
+    id: "player_kurihara",
+    display_text: "4番 サードベースマン 栗原陵矢<br>背番号24 福井県福井市出身",
+    typing_kana: "よんばん さーどべーすまん くりはらりょうや<br>せばんごうにじゅうよん ふくいけんふくいししゅっしん",
+    bgm_file: "audio/kurihara.mp3",
+    bgm_delay_ms: 2000
+  },
+  {
+    id: "player_yanagita",
+    display_text: "5番 指名打者 柳田悠岐<br>背番号9 広島県広島市出身",
+    // ※指名打者は「デジグネイテッドヒッター(DH)」とするか迷いましたが、日本の球場アナウンスで一般的な「しめいだしゃ」のままにしています。
+    // もし変更したい場合は「でじぐねいてっどひったー」に書き換えてください。
+    typing_kana: "ごばん しめいだしゃ やなぎたゆうき<br>せばんごうきゅう ひろしまけんひろしまししゅっしん",
+    bgm_file: "audio/yanagita.mp3",
+    bgm_delay_ms: 2000
+  }
+];
